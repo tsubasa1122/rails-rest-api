900d9b9eb4156261dad2cec3e736938192c84de7
tsubasa1122 <nakanot@open8.com>
2019-11-03 21:45:11 +0900

    facebook認証作成

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,4 +1,4 @@
 Koala.configure do |config|
   config.app_id = Rails.application.credentials.facebook[:app_id]
-  config.app_secret = Rails.application.credentials.facebook[:app_secret]Z
+  config.app_secret = Rails.application.credentials.facebook[:app_secret]
 end
