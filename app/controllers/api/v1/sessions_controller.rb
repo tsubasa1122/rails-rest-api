2676e57702724d037c8facaf46f846eb8f182942
tsubasa1122 <nakanot@open8.com>
2019-11-03 19:38:01 +0900

    logout機能実装

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   def create
     if @user.valid_password?(sign_in_params[:password])
@@ -8,6 +10,12 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response("Unauthorized", false, {}, :unauthorized)
     end
+  end
+
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response("Log out Successfully", true, {}, :ok)
   end
 
   private
@@ -22,6 +30,15 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response("Cannot get User", false, {}, :failure)
+    end
+  end
+
+  def valid_token
+    @user = User.find_by(authentication_token: request.headers["AUTH-TOKEN"])
+    if @user
+      return @user
+    else
+      json_response("Invalid token", false, {}, :failure)
     end
   end
 end
