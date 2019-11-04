e58b9e37feba2d74f85408f2f51ba4246ab5f066
tsubasa1122 <nakanot@open8.com>
2019-11-04 15:15:13 +0900

    add paperclip

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,22 @@
 class Review < ApplicationRecord
+  before_validation :parse_image
   before_save :calculate_avarage_rating
   belongs_to :user
   belongs_to :book
 
   counter_culture :book
+
+  attr_accessor :image_review
+
+  has_attached_file :picture, sytles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment :picture, presence: true
+  do_not_validate_attachment_file_type :picture
+
+  def parse_image
+    image = Paperclip.io_adapters.for(image_review)
+    image.original_filename = "review_image.jpg"
+    self.picture = image
+  end
 
   def calculate_avarage_rating
     self.average_rating = ((self.content_rating.to_f + self.recommend_rating.to_f) / 2).round(1) 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_050526) do
+ActiveRecord::Schema.define(version: 2019_11_04_054643) do
 
   create_table "books", force: :cascade do |t|
     t.string "image"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 2019_11_04_050526) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
